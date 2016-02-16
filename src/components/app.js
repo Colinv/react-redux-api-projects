6b988b06468bd2321a4e3cb1a3b6eb07cf4af957
George Pickett <gpickett00@gmail.com>
2016-02-15 22:24:44 -0800

    initial commit?

---
+++ src/components/app.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Component } from 'react';
+import SearchBar from '../containers/search_bar';
+import ApiContainer from '../containers/api_container';
+
+export default class App extends Component {
+	render(){
+		return(
+			<div className="outside">
+				<SearchBar />
+				
+				<div className="main-box">
+					<ApiContainer />
+
+				</div>
+				
+			</div>
+		);
+
+	}
+}
